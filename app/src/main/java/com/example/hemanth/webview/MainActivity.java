9528479a31a2d98ab8898cb4b37adf8fad38b0ce
hemant314 <hemant@designstring.com>
2016-10-24 18:07:02 +0530

    First push

---
+++ app/src/main/java/com/example/hemanth/webview/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.hemanth.webview;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    WebView webView;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initialize webview
+        webView = (WebView) findViewById(R.id.webView);
+
+        //Enable javascript to load web page
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+        //to load web page with url
+        webView.loadUrl("http://www.designstring.com/");
+    }
+
+
+
+
+    public class WebViewClientImpl extends WebViewClient {
+
+        private Activity activity = null;
+
+
+        public WebViewClientImpl(Activity activity) {
+            this.activity = activity;
+        }
+
+
+        //Show loader on url load
+        public void onLoadResource (WebView view, String url) {
+            if (progressDialog == null) {
+                // in standard case YourActivity.this
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Loading...");
+                progressDialog.show();
+            }
+        }
+        public void onPageFinished(WebView view, String url) {
+            try{
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+
+                }
+            }catch(Exception exception) {
+                exception.printStackTrace();
+            }
+
+        }
+
+        //When the shouldOverrideUrlLoading() method returns false, the URLs passed as parameter to the method is loaded inside the WebView instead of the browser.
+
+        //To distinguish between the URLs that are loaded within the app and browser the following code needs to be added in the shouldOverrideUrlLoading() method:
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            if(url.indexOf("designstring.com") > -1 ) return false;
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            activity.startActivity(intent);
+            return true;
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
